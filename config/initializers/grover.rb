154faa03d97199dfd5cc3edf7d1eb7095b30b695
Enrico Gigantiello <enrico.gigantiello@leanbakery.com>
2026-03-23 21:09:03 +0100

    use chromium path only in production

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,6 +1,8 @@
-Grover.configure do |config|
-  config.options = {
-    executable_path: "/usr/bin/chromium",
-    launch_args: [ "--no-sandbox", "--disable-setuid-sandbox" ]
-  }
+if Rails.env.production?
+  Grover.configure do |config|
+    config.options = {
+      executable_path: "/usr/bin/chromium",
+      launch_args: [ "--no-sandbox", "--disable-setuid-sandbox" ]
+    }
+  end
 end
